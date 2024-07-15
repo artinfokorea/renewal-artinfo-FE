ef39661ee3bbca54243dfd80c77533f6bc5f3f31
baek <bgh9651@gmail.com>
2024-07-15 14:52:13 +0900

    Upt. job skeleton fixed

--- components/skeleton/JobListSkeleton.tsx
+++ components/skeleton/JobListSkeleton.tsx
@@ -6,9 +6,9 @@ const JobListSkeleton = () => {
       {Array.from({ length: 10 }, (_, index) => index).map(index => (
         <div
           key={index}
-          className="my-4 flex h-[114px] rounded-xl py-4 md:h-[174px]"
+          className="m-4 flex h-[130px] rounded-xl py-4 md:h-[192px]"
         >
-          <div className="w-[120px] animate-pulse rounded-lg bg-gray-200 md:w-[230px]" />
+          <div className="h-full w-[120px] animate-pulse rounded-lg bg-gray-200 md:w-[230px]" />
           <div className="ml-4 flex flex-1 flex-col justify-center py-2 md:ml-12">
             <div className="mb-2 h-5 w-40 animate-pulse bg-gray-200" />
             <div className="mb-2 h-5 w-24 animate-pulse bg-gray-200" />
