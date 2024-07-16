8adf643a1ccc07ca60c63895e15b9a6bf25b3779
baek <bgh9651@gmail.com>
2024-07-16 14:15:03 +0900

    Upt. newsCount

--- components/news/NewsContainer.tsx
+++ components/news/NewsContainer.tsx
@@ -9,17 +9,27 @@ import { UserType } from "@/types/users"
 import { Button } from "../ui/button"
 import { usePathname, useRouter } from "next/navigation"
 import NewsSkeleton from "../skeleton/NewsSkeleton"
+import { useSession } from "next-auth/react"
 
 const NewsContainer = () => {
-  const { data: user } = useQuery(queries.users.detail())
   const router = useRouter()
   const pathname = usePathname()
+  const { data } = useSession()
+
+  const { data: user } = useQuery({
+    ...queries.users.detail(),
+    enabled: !!data?.user,
+  })
+  const { data: newsCount } = useQuery(queries.news.count())
 
   return (
     <div className="mx-auto max-w-screen-lg">
       <ListSearchForm>
         <h4 className="text-xl font-bold md:text-2xl">
-          아트인포 예술 뉴스 <span className="pl-[2px] text-main">00</span>
+          아트인포 예술 뉴스{" "}
+          <span className="pl-[2px] text-main">
+            {newsCount?.totalCount || "00"}
+          </span>
         </h4>
       </ListSearchForm>
       {user?.type === UserType.ADMIN && (
