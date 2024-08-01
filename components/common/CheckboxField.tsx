4006c1cae030085723e0798d060b905b172168f8
baek <bgh9651@gmail.com>
2024-08-01 10:05:46 +0900

    Upt. checkboxes color change

--- components/common/CheckboxField.tsx
+++ components/common/CheckboxField.tsx
@@ -26,12 +26,12 @@ const CheckboxField = <T,>({
         type="checkbox"
         value={value as string | number}
         checked={checked}
-        className="peer h-[22px] w-[22px] appearance-none rounded-md border-2 border-gray-300 checked:flex checked:items-center checked:justify-center checked:border-none checked:bg-main checked:before:absolute checked:before:text-2xl checked:before:font-bold checked:before:text-white checked:before:content-['✓']"
+        className="peer h-[22px] w-[22px] appearance-none rounded-md border-2 border-silver checked:flex checked:items-center checked:justify-center checked:border-none checked:bg-main checked:before:absolute checked:before:text-2xl checked:before:font-bold checked:before:text-white checked:before:content-['✓']"
         onChange={() => handleChange(value!)}
       />
       <Label
         htmlFor={title}
-        className={`peer-checked:text-blue-500 ${
+        className={`text-silver peer-checked:text-blue-500 ${
           child ? "text-base font-medium" : "text-lg font-semibold"
         }`}
       >
