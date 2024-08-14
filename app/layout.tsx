fd0619674d9d669965373b542735451d6181a0f5
baek <bgh9651@gmail.com>
2024-08-14 14:41:11 +0900

    Upt. script strategy fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,11 +86,11 @@ export default function RootLayout({
         />
         <Script
           src="https://developers.kakao.com/sdk/js/kakao.js"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         <Script
           src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         <ClientProvider>{children}</ClientProvider>
       </body>
